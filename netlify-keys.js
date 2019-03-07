0f65b941320c8f3f510ff23d95ada3fc1d24e98a
Elvis <elviswolcott@gmail.com>
2019-03-07 17:44:26 -0600

    remove environment variable test (it's working)

--- netlify-keys.js
+++ netlify-keys.js
@@ -2,10 +2,4 @@ const fs = require('fs');
 
 console.log("Creating keys.json from environment variables.");
 
-console.log("Environment variable test");
-
 fs.writeFileSync('./keys.json', JSON.stringify(process.env));
-
-console.log("Checking file contents");
-const output = require('./keys.json');
-console.log(output.publishKey, output.subscribeKey);
