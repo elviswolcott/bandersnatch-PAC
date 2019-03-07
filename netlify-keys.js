d0107404ac841eb49176a810f7a68dc55c5dc300
Elvis <elviswolcott@gmail.com>
2019-03-07 17:40:45 -0600

    another environment variable check

--- netlify-keys.js
+++ netlify-keys.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 console.log("Creating keys.json from environment variables.");
 
 console.log("Environment variable test");
-console.log(JSON.stringify(process.env));
 
 fs.writeFileSync('./keys.json', JSON.stringify(process.env));
+
+console.log("Checking file contents");
+const output = require('./keys.json');
+console.log(output.publishKey, output.subscribeKey);
