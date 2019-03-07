532cf655de55e23c0dccad66d82418fe4f04b10b
Elvis <elviswolcott@gmail.com>
2019-03-07 17:34:38 -0600

    quick test to make sure environment variables is working with netlify

--- netlify-keys.js
+++ netlify-keys.js
@@ -2,4 +2,7 @@ const fs = require('fs');
 
 console.log("Creating keys.json from environment variables.");
 
+console.log("Environment variable test");
+console.log(JSON.stringify(process.env));
+
 fs.writeFileSync('./keys.json', JSON.stringify(process.env));
